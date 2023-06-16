8d00f00ee196d75884e3d3b584d769dfd3b06981
maquico <1104666@est.intec.edu.do>
2023-06-15 22:38:58 -0400

    mongoose en test

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,29 +1,33 @@
 const request = require('supertest');
 const app = require('../src/server');
+const mongoose = require('mongoose');
 
 let server;
-let shortUrl;
-const fullUrl = 'https://youtube.com'; // Replace with the full URL you want to test
 
 beforeAll(async () => {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
   server = await app.listen(3000);
 });
 
 afterAll(async () => {
+  await mongoose.connection.close();
   await server.close();
 });
 
 describe('Link Shortener', () => {
   describe('POST /shorten', () => {
     it('should generate a short URL', async () => {
-      // Insert the URL and generate a short URL
+      const fullUrl = 'https://www.youtube.com/';
       const response = await request(app)
         .post('/shorten')
         .send({ fullUrl });
 
       expect(response.status).toBe(201);
       expect(response.body).toHaveProperty('shortUrl');
-      shortUrl = response.body.shortUrl;
     });
   });
 });
+
