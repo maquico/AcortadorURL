22481e1c9ea5e01b1eec184f913bc36fb2800025
maquico <1104666@est.intec.edu.do>
2023-06-15 22:17:42 -0400

    timeout

--- tests/server.test.js
+++ tests/server.test.js
@@ -2,11 +2,12 @@ const request = require('supertest');
 const app = require('../src/server');
 
 let server;
-let shortUrl;
+const fixedShortUrl = 'abcd'; // Replace with a fixed short URL
+const expectedFullUrl = 'https://youtube.com'; // Replace with the expected full URL
 
 beforeAll(async () => {
   server = await app.listen(3000);
-}, 10000);
+});
 
 afterAll(async () => {
   await server.close();
@@ -14,18 +15,11 @@ afterAll(async () => {
 
 describe('Link Shortener', () => {
   describe('GET /:shortUrl', () => {
-    beforeAll(async () => {
-      // Perform the necessary steps to generate the short URL and store it in the `shortUrl` variable
-      const inputUrl = 'https://youtube.com'; // Replace with your desired input URL
-      const response = await request(app).post('/shortUrls').send({ fullUrl: inputUrl });
-      shortUrl = response.body.shortUrl; // Assuming the generated short URL is returned in the response body
-    });
-
     it('should redirect to the full URL', async () => {
-      const response = await request(app).get(`/${shortUrl}`);
+      const response = await request(app).get(`/${fixedShortUrl}`);
 
       expect(response.status).toBe(302);
-      expect(response.header['location']).toBe('https://youtube.com'); // Replace with the expected full URL
-    });
+      expect(response.header['location']).toBe(expectedFullUrl);
+    }, 10000); // Set a timeout of 10 seconds (adjust as needed)
   });
 });
