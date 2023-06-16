a7a3e4c4bf882ca5f31a03b3983c996242357e7d
maquico <1104666@est.intec.edu.do>
2023-06-15 21:56:42 -0400

    unit test v3

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,14 +1,23 @@
 const request = require('supertest');
 const app = require('../src/server');
 
+let server;
+
+beforeAll(async () => {
+  server = await app.listen(5000);
+});
+
+afterAll(async () => {
+  await server.close();
+});
+
 describe('Link Shortener', () => {
   describe('GET /:shortUrl', () => {
     it('should redirect to the full URL', async () => {
-      const response = await request(app).get('/abcd'); // replace "abcd" with an existing short URL in your database
+      const response = await request(app).get('/existing-short-url');
 
-      expect(response.status).toBe(302); // assuming you expect a redirect response
-      expect(response.header['location']).toBe('https://example.com'); // replace with the expected full URL
-      // additional assertions if needed
+      expect(response.status).toBe(302);
+      expect(response.header['location']).toBe('https://example.com'); // Replace with the expected full URL
     });
   });
 });
